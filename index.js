a24f469eaf11f5765f5b4b065e86f118bb58614f
Jack Atkerson <jatkerson18@gmail.com>
2022-06-26 16:51:08 -0500

    Code finished minus styling. Trying to fix node error

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+const manager = require('Manager');
 const Engineer = require('Engineer');
 const Intern = require('Intern');
-const Manager = require('Manager');
 const generateHTML = require('./src/generateHTML');
 
 const employeeArray = [];
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -58,9 +58,61 @@ function generateIntern(intern) {
     `
 };
 
-function generateHTML(employees) {
+generateHTML = (data) => {
+    pageArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole();
+
+        if (role === 'Manager') {
+            const managerCard = generateManager(employee);
+            pageArray.push(managerCard);
+        }
+
+        if (role === 'Engineer') {
+            const engineerCard = generateEngineer(employee);
+            pageArray.push(engineerCard);
+        }
+
+        if (role === 'Intern') {
+            const internCard = generateIntern(employee);
+            pageArray.push(internCard);
+        }
+    }
+
+    const employeeCard = pageArray.join('')
+
+
+};
+
+const generateEmployeePage = function (employeeCards) {
     return `
-    `;
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link rel="stylesheet" href="style.css">
+    <body>
+        <header>
+            <nav class="navbar">
+                <span>My Team</span>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-center" id="cards">
+                    ${employeeCards}
+                </div>
+            </div>
+        </main>
+    </body>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </html>
+
+  `
 };
 
 module.exports = generateHTML;  
